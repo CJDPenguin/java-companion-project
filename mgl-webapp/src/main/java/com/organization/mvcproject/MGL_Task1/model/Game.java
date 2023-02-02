4b64018eec61af554eb09799903afedfe7b4be0e
chris.a.davis <chris.a.davis@AFSWWK6NTSYEXO.us.accenturefederal.com>
2023-02-02 09:42:29 -0600

    Model refactoring done

--- mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/model/Game.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/model/Game.java
@@ -5,36 +5,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class Game {
 
-	/**
-	 * TODO 1.0 java object member variable naming convention declared here are not object oriented,
-	 *  rename the variables, and their setter/getter methods to match convention. 
-	 */
 	private Long id;
 	private String name;
 	private String genre;
 
-	public Long getGame_id() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setGame_id(Long game_id) {
-		this.id = game_id;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
-	public String getGame_name() {
+	public String getName() {
 		return name;
 	}
 
-	public void setGame_name(String game_name) {
-		this.name = game_name;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public String getGame_genre() {
+	public String getGenre() {
 		return genre;
 	}
 
-	public void setGame_genre(String game_genre) {
-		this.genre = game_genre;
+	public void setGenre(String genre) {
+		this.genre = genre;
 	}
 
 }
